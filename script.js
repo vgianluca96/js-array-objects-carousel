543f2b499bd734f88ae7350c1244ddb4a9d28660
vgianluca96 <gianluca.vallese@hotmail.com>
2023-09-07 15:13:16 +0200

    carousel v1
    create card (solo la prima visibile) e bottoni avanti/indietro

--- script.js
+++ script.js
@@ -1,3 +1,43 @@
 
 
-console.log('ciao');
+let imgNumber = 5;
+let carouselArray = [];
+let carousel = document.getElementById('carousel');
+
+for (i = 1; i <= imgNumber; i++) {
+
+    let carouselArrayElem = {
+        path: 'https://picsum.photos/400/300?random=' + i,
+        title: 'Image ' + i,
+        text: 'Image taken from Lorem Picsum'
+    }
+    carouselArray.push(carouselArrayElem);
+
+    carousel.innerHTML += `
+    <div id="carouselCard${i}" class="card">
+
+            <img src="${carouselArrayElem.path}" class="card-img-top">
+    
+            <div class="card-body">
+
+                <h5 class="card-title">${carouselArrayElem.title}</h5>
+
+                <p class="card-text">${carouselArrayElem.text}</p>
+    
+            </div>
+    
+        </div>
+    `;
+
+    if (i != 1) {
+
+        let carouselCard = document.getElementById('carouselCard' + i);
+
+        //console.log(carouselCard.classList);
+        carouselCard.classList.add('d-none');
+
+    }
+
+}
+
+console.log(carouselArray);
